ace2661525114904d4635eaf9110e60cca60c37a
soniaakter09 <sa0308355@gmail.com>
2024-03-24 13:36:35 +0600

    protfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-var sonia = 10/2;
+// function projects(){
+// fetch('')
+// .than(res => res.json())
+// .than(data => console.log(data))
 
-console.log(sonia);
+// }
+function loadProject(){
+    fetch('./project.json')
+    .then(res => res.json())
+    .then(data => displayProject(data))
+}
+loadProject();
+
+function displayProject(data) {
+    const box = document.getElementById('project')
+
+    for(const protfolio of data){
+      console.log(protfolio);
+        const div = document.createElement('div');
+        div.innerHTML = `
+    
+        
+      
+        <div class="card" style="width: 22rem;">
+          <img src="${protfolio.images}" class="card-img-top" alt="..."> 
+          <h4>${protfolio.skill}</h4>  
+        </div>
+      
+
+   
+    
+    
+
+  
+        `;
+       box.appendChild(div);
+    }
+}
